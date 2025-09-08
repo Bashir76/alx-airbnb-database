1a2f99eb33a0d2971c163aba76b52c2a3736c306
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2025-09-08 10:19:42 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,24 +1,12 @@
--- 1. AGGREGATION QUERY
--- Find the total number of bookings made by each user
-SELECT 
-    u.user_id,
-    u.username,
-    COUNT(b.booking_id) AS total_bookings
+-- Query 1: Find the total number of bookings made by each user
+SELECT u.user_id, u.username, COUNT(b.booking_id) AS total_bookings
 FROM users u
-LEFT JOIN bookings b 
-    ON u.user_id = b.user_id
+LEFT JOIN bookings b ON u.user_id = b.user_id
 GROUP BY u.user_id, u.username;
 
-
--- 2. WINDOW FUNCTION QUERY
--- Rank properties based on the total number of bookings they have received
-SELECT 
-    p.property_id,
-    p.property_name,
-    COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
+-- Query 2: Rank properties based on the total number of bookings they have received
+SELECT p.property_id, p.property_name, COUNT(b.booking_id) AS total_bookings,
+       RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
 FROM properties p
-LEFT JOIN bookings b 
-    ON p.property_id = b.property_id
+LEFT JOIN bookings b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.property_name;
-
