2fdb04eedb8d46596688254017b92b4e7658898b
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2025-09-08 10:37:55 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -5,8 +5,9 @@ LEFT JOIN bookings b ON u.user_id = b.user_id
 GROUP BY u.user_id, u.username;
 
 -- Query 2: Rank properties based on the total number of bookings they have received
-SELECT p.property_id, p.property_name, COUNT(b.booking_id) AS total_bookings,
-       ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
+SELECT p.property_id, p.property_name, COUNT(b.booking_id) AS row_number_rank,
+       ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property rank, 
+       RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
 FROM properties p
 LEFT JOIN bookings b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.property_name;
